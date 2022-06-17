467b00942d2945967f71aff3d7903e686998230a
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2022-06-17 14:28:16 -0500

    a simple network

--- simple_network.py
+++ simple_network.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn
 from torch.autograd import Variable
 
+"""
 def prep_param_lists(model):
   '''
   Helper Functions
@@ -18,24 +19,28 @@ def master_params_to_model_params(model_params, master_params):
   for model, master in zip(model_params, master_params):
       model.data.copy_(master.data)
       model.data.copy_(master.data)
+"""
 
 
 N, D_in, D_out = 64, 1024, 512
 
-x = Variable(torch.randn(N, D_in)).cuda().half()
-y = Variable(torch.randn(N, D_out)).cuda().half()
+#x = Variable(torch.randn(N, D_in)).cuda().half()
+#y = Variable(torch.randn(N, D_out)).cuda().half()
+x = Variable(torch.randn(N, D_in)).cuda()
+y = Variable(torch.randn(N, D_out)).cuda()
 
-model = torch.nn.Linear(D_in, D_out).cuda().half()
-model_params, master_params = prep_param_lists(model)
+model = torch.nn.Linear(D_in, D_out).cuda()
+# model_params, master_params = prep_param_lists(model)
 
-optimizer = torch.optim.SGD(master_params, lr=1e-3)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 for t in range(500):
   y_pred = model(x)
 
   loss = torch.nn.functional.mse_loss(y_pred, y)
 
-  model.zero_grad()
+  # model.zero_grad()
+  optimizer.zero_grad()
   loss.backward()
   optimizer.step()
 
